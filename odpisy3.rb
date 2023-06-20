f5ff88b49f09ee12859d7dbca343e9cfddfde69e
Felyp <janichfilip@seznam.cz>
2023-06-20 21:47:28 +0200

    sneeeze kokot

--- odpisy3.rb
+++ odpisy3.rb
@@ -4,7 +4,7 @@ loop do
         num_groups = whole.chars.to_a.reverse.each_slice(3)
         whole_with_commas = num_groups.map(&:join).join(',').reverse
         [whole_with_commas, decimal].compact.join(".")
-      end
+    end
     
 
 class Odpisy
